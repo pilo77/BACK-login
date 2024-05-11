a415e0b2b700ac73a9c66e8a35e944177e31c03b
pilo77 <cavillamil-2022a@corhuila.edu.co>
2024-05-11 16:43:43 -0500

    añadiendo contenido en el UserController.java

--- loginback/src/main/java/com/example/loginback/Security/Controller/UserController.java
+++ loginback/src/main/java/com/example/loginback/Security/Controller/UserController.java
@@ -1,4 +1,55 @@
 package com.example.loginback.Security.Controller;
 
+
+import com.example.loginback.Security.Dto.UserDTO;
+import com.example.loginback.Security.Request.UserRequest;
+import com.example.loginback.Security.Response.UserResponse;
+import com.example.loginback.Security.Service.UserService;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RestController
+@PreAuthorize("hasRole('ADMIN')")
+@RequestMapping(value = "/api/v1/user")
+
+@RequiredArgsConstructor
+
 public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/all")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<List<UserDTO>> getAllUsers() {
+        List<UserDTO> users = userService.getAllUsers(); // Asegúrate de obtener la lista de usuarios
+        return ResponseEntity.ok(users); // Devuelve la lista de usuarios
+    }
+
+    @GetMapping(value = "{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<UserDTO> getUser(@PathVariable Integer id) {
+        UserDTO userDTO = userService.getUser(id);
+        if (userDTO == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(userDTO);
+    }
+
+    @PutMapping("/modificar")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<UserResponse> updateUser(@RequestBody UserRequest userRequest) {
+        return ResponseEntity.ok(userService.updateUser(userRequest));
+    }
 }
