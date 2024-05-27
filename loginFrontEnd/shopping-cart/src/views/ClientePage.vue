2df760f6124b7501dc0c4e125f4b0d43d4f3fe8d
Camilo-02 <camiloandreschavarroguenis@gmail.com>
2024-05-27 09:47:39 -0500

    cambios frontEnd

--- loginFrontEnd/shopping-cart/src/views/ClientePage.vue
+++ loginFrontEnd/shopping-cart/src/views/ClientePage.vue
@@ -9,14 +9,16 @@
         <div id="login-container">  
           <div id="login-form">
             <InputComponent id="id" name="id" type="hidden"/>
-            <InputComponent id ="nombre" name = "nombre" label ="Nombre: "></InputComponent>
-            <InputComponent id="tipoDocumento" name="tipoDocumento" label="Tipo Documento: " />
-            <InputComponent id="nombre" name="nombre" label="Contraseña: " />          
+            <InputComponent id ="country" name = "country" label ="Nacionalidad: "/>
+            <InputComponent id ="firstname" name = "firstname" label ="Nombres: "/> 
+            <InputComponent id ="lastname" name = "lastname" label ="Apellidos: "/>
+            <InputComponent id ="username" name = "username" label = "Usuario: "/>
+            <InputComponent id="password" name="password" label="Contraseña: " />          
           </div>  
           <br>
           <br>
           <div> 
-            <ButtonComponent id="crear" value="Crear Usuario" expand="full"></ButtonComponent>
+            <ButtonComponent id="crear" value="Crear Usuario" expand="full" nameMethod="createRecord" @click="createRecord"></ButtonComponent>
             <ButtonComponent id = "select" value = "atras" expand="full" color="danger" @click="atras"></ButtonComponent>
 
           </div>          
@@ -24,15 +26,41 @@
       </ion-content>
     </ion-page>
   </template>
+
   
   <script setup lang="ts">
   import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
   import InputComponent from '@/components/InputComponent.vue';
   import ButtonComponent from '@/components/ButtonComponent.vue';
+  import axios from 'axios';
+
+  const baseURL ="http://localhost:9000/recordatorioG/auth/register"
+
 
   const atras = () => {
      window.location.href = "/login"
   }
+
+
+async function createRecord() {
+  const data = {
+    
+    country: country.value,
+    firstname: firstname.value,
+    lastname: lastname.value,
+    username: username.value,
+    password: password.value
+
+  };
+
+  try {
+    const response = await axios.post(baseURL, data);
+    console.log('Registro creado exitosamente:', response.data);
+    window.location.href = "/login"
+  } catch (error) {
+    console.error('Error al crear el registro:', error);
+  }
+}
 
   </script>
   
--- loginFrontEnd/shopping-cart/src/views/LoginPage.vue
+++ loginFrontEnd/shopping-cart/src/views/LoginPage.vue
@@ -1,53 +1,78 @@
 <template>
-    <ion-page>
-      <ion-header :translucent="true">
-        <ion-toolbar>
-          <ion-title class="cart-title">LOGIN</ion-title>
-        </ion-toolbar>
-      </ion-header>
-  
-      <div id="login-container">
-        <div id="login-form" class="login-form">
-          <strong>Bienvenido al Recordatorio Personal</strong>
-          <br>
-          <br>
-          <InputComponent v-model="user" id="user" name="user" label="Usuario: " placeholder="Introduce tu usuario" />
-          <InputComponent v-model="password" type="password" id="password" name="password" label="Contraseña: " placeholder="Introduce tu contraseña" />
-        </div>
-        <div class="login-button">
-          <ButtonComponent id="login" value="Iniciar Sesión" expand="full" @click="login" />
-          <ButtonComponent id="forgotPassword" value="Crear Cuenta" expand="full" @click= "newUser"/>
-        </div>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-title class="cart-title">LOGIN</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <div id="login-container">
+      <div id="login-form" class="login-form">
+        <strong>Bienvenido al Recordatorio Personal</strong>
+        <br>
+        <br>
+        <InputComponent v-model="username" id="username" name="username" label="Usuario: " placeholder="Introduce tu usuario" />
+        <InputComponent v-model="password" id="password" name="password" label="Contraseña: " placeholder="Introduce tu contraseña" />
       </div>
-    </ion-page>
-  </template>
-  
-  <script setup lang="ts">
-  import { ref } from 'vue';
-  import { IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-  import ButtonComponent from '@/components/ButtonComponent.vue';
-  import InputComponent from '@/components/InputComponent.vue';
+      <div class="login-button">
+        <ButtonComponent id="login" value="Iniciar Sesión" expand="full" color="dark" @click="login" />
+        <ButtonComponent id="forgotPassword" value="Crear Cuenta" expand="full" color="success" @click="newUser" />
+      </div>
+    </div>
+  </ion-page>
+</template>
 
-  const user = ref('');
-  const password = ref('');
-  
-  const login = () => {
-    const username = user.value;
-    const pass = password.value;
-  
-    if (username === 'abcd' && pass === 'abcd') {      
-      window.location.href = "/dashboard";
-    } else {
-      console.log('Credenciales inválidas');
-    }
-  };
+<script setup lang="ts">
+import { ref } from 'vue';
+import { IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import ButtonComponent from '@/components/ButtonComponent.vue';
+import InputComponent from '@/components/InputComponent.vue';
+import axios from 'axios';
 
-  const newUser = () => {
-     window.location.href = "/cliente"
+
+
+
+const baseURL = 'http://localhost:9000/recordatorioG/auth/login';
+
+const username = ref('');
+const password = ref('');
+
+
+
+const newUser = () => {
+  window.location.href = '/cliente';
+};
+
+
+const login = async () => {
+
+  if (!username.value || !password.value) {
+    alert('Por favor, ingrese su usuario y contraseña');
+    return;
   }
 
+  try {
+    const response = await axios.post(baseURL, {
+      username: username.value,
+      password: password.value,
+    });
 
-  </script>
+    const { token, role } = response.data;
+
+    localStorage.setItem('token', token);
+
   
-  <style scoped src="../theme/login.css"></style>
-  
+    if (role === 'ADMIN') {
+      window.location.href = '/administrador';
+    } else if (role === 'USER') {
+      window.location.href = '/dashboard';
+    } else {
+      console.error('Rol no reconocido:', role);
+    }
+  } catch (error) {
+    console.error('Error en el inicio de sesión:', error);
+  }
+};
+</script>
+
+<style scoped src="../theme/login.css"></style>
