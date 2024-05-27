fa89745eb83312f79482099a14403f4e0985d014
pilo77 <cavillamil-2022a@corhuila.edu.co>
2024-05-26 19:54:25 -0500

    Services de categora y recordatorio

--- loginback/src/main/java/com/example/loginback/IService/CategoriaService.java
+++ loginback/src/main/java/com/example/loginback/IService/CategoriaService.java
@@ -1,4 +1,18 @@
 package com.example.loginback.IService;
 
-public class CategoriaService {
+import com.example.loginback.Entity.Categoria;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CategoriaService {
+    Categoria save(Categoria categoria);
+
+    void update(Categoria categoria, Long id);
+
+    void delete(Long id);
+
+    Optional<Categoria> findById(Long id);
+
+    List<Categoria> findAll();
 }
