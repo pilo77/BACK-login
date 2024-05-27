a79367825accd3c984e0585c9118c506395bd0cb
pilo77 <cavillamil-2022a@corhuila.edu.co>
2024-05-27 09:49:36 -0500

    logica si es recodatorio estA ACTIVO para mostrarlo

--- loginback/src/main/java/com/example/loginback/Security/Authlogin/Config/SecurityConfig.java
+++ loginback/src/main/java/com/example/loginback/Security/Authlogin/Config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
                                 .requestMatchers("/v3/api-docs/**").permitAll()
                                 .requestMatchers("/swagger-ui/**").permitAll()
                                 .requestMatchers("/swagger-ui.html").permitAll()
-                                .requestMatchers("/api/v1/user/**").hasAuthority("ADMIN")
+                                .requestMatchers("/api/user/**").hasAuthority("ADMIN")
                                 .requestMatchers("/api/v1/demo/**").hasAuthority("USER")
 
                                 .requestMatchers("/auth/**").permitAll()
